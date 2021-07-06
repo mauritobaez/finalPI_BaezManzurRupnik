09acdce762d97e72cd201cdd818831c2d5649930
Franco Rupnik <francorupnik@gmail.com>
2021-07-06 16:00:55 -0300

    Adicion de readLine y textToNum a fileReading.c

--- fileReading.h
+++ fileReading.h
@@ -1,7 +1,12 @@
 #ifndef TP_FINAL_PI_FILEREADING_H
 #define TP_FINAL_PI_FILEREADING_H
-
-
+#define MAX_LONG 256
+#define DELIM ";"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+char ** readLine(FILE* input,size_t dim);
+size_t textToNum(char * text);
 
 
 #endif //TP_FINAL_PI_FILEREADING_H
