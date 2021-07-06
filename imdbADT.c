2bc7dd6a2873162de39b685e88ef97373425febb
mauritobaez <mauroleanbaez@gmail.com>
2021-07-06 13:53:08 -0300

    Estructura ADT básica

---
+++ imdbADT.c
@@ -0,0 +1,35 @@
+
+#include <stdlib.h>
+//lista para generos (query2)
+typedef struct node{
+    char * genre;
+    struct node * tail;
+    size_t count;
+}TNode;
+
+typedef TNode * TList;
+
+//para query3
+typedef struct best{
+    char * name;
+    float rating;
+    size_t votes;
+}best;
+
+//información del año
+typedef struct year{
+    size_t amountSeries;
+    size_t amountMovies;
+    struct best bestMovie;
+    struct best bestSeries;
+    TList firstGenre;
+    TList currentGenre;
+}year;
+
+// nuestro CDT
+typedef struct imdbCDT{
+    year ** yearsAfter;
+    year ** yearsBefore;
+    size_t size;
+    size_t yearZero;
+}imdbCDT;
