86443001db06c48cb815d24565bdfa2d88c063e2
MatyManzur <mmanzur@itba.edu.ar>
2021-07-07 13:21:05 -0300

    Adición de macro CHECK_ALLOC

--- auxFunctions.c
+++ auxFunctions.c
@@ -8,11 +8,13 @@ char * copyString(const char * string){
     while(*string != '\0'){
         if(dim%BLOCK == 0){
             aux = realloc(aux, dim + BLOCK);
+            CHECK_ALLOC(aux);
         }
         aux[dim++] = *string;
         string++;
     }
     aux = realloc(aux, dim+1);
+    CHECK_ALLOC(aux);
     aux[dim] = '\0';
     return aux;
 }
--- auxFunctions.h
+++ auxFunctions.h
@@ -1,6 +1,13 @@
 #ifndef TP_FINAL_PI_AUXFUNCTIONS_H
 #define TP_FINAL_PI_AUXFUNCTIONS_H
 
+#include <stdio.h>
+
+#define CHECK_ALLOC(x) {    if(x==NULL)\
+                            {  fprintf(stderr, "Error. No more memory available.");\
+                                exit(3);\
+                            }\
+                        }
 char * copyString(const char * string);
 
 
